19124b4bb1af7a5b5e5545ca358c9735fb13afbe
James Oddy <jamesoddy8@gmail.com>
2020-02-13 13:48:27 +0000

    fizzbuzz pairing rspec completed

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -1,7 +1,17 @@
 require 'fizzbuzz'
 
 describe 'fizzbuzz' do
+
   it 'returns "fizz" when passed 3' do
     expect(fizzbuzz(3)).to eq 'fizz'
+  end
+  it 'returns "buzz" when passed 5' do
+    expect(fizzbuzz(5)).to eq 'buzz'
+  end
+  it 'returns "fizz" when passed multiple of 5 & 3' do
+    expect(fizzbuzz(15)).to eq 'fizzbuzz'
+  end
+  it 'returns "number" when no condition applied' do
+    expect(fizzbuzz(4)).to eq 4
   end
 end
